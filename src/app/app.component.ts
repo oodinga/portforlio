51b07f5908c50b54f9611b98cc87721a50198f67
odinga96 <divadagnido96@gmail.com>
2024-11-18 12:54:43 +0300

    Portfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Project, Skill } from './models/models';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,51 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'portfolio';
+
+  fileUrl = 'assets/resume.pdf';
+
+  skills: Skill[] = [
+    {
+      languages: ["Node.js", "Python", "Go", "Java"],
+      name: "Backend"
+    },
+    {
+      languages: ["HTML", "CSS", "JS", "Angular"],
+      name: "Frontend"
+    },
+    {
+      languages: ["MySQL", "SQL", "MongoDB", "PostgreSql"],
+      name: "Database"
+    }
+  ]
+
+  projects: Project[] =[
+    {
+      description: "A full-stack social media platform built with Angular and Node.js.",
+      imageUrl: "assets/bg.avif",
+      link: "",
+      title: "Social Media Platform"
+    },
+    {
+      description: "An e-commerce application with payment gateway integration.",
+      imageUrl: "assets/bg.avif",
+      link: "",
+      title: "E-commerce Store"
+    },
+    {
+      description: "A real-time chat application using WebSockets and Node.js.",
+      imageUrl: "assets/bg.avif",
+      link: "",
+      title: "Real-Time Chat App"
+    },
+  ]
+  downloadFile() {
+    // Create an invisible anchor element
+    const anchor = document.createElement('a');
+    anchor.href = this.fileUrl;
+    anchor.download = 'DavidOdinga_Resume.pdf';  // Set the file name for download
+    document.body.appendChild(anchor);  // Attach the anchor to the DOM
+    anchor.click();  // Programmatically trigger the click event
+    document.body.removeChild(anchor);  // Remove the anchor after download
+  }
 }
---
+++ src/app/models/models.ts
@@ -0,0 +1,11 @@
+export interface Skill{
+    name: string;
+    languages: string[];
+}
+
+export interface Project{
+    title?: string;
+    link?: string;
+    description?: string;
+    imageUrl?: string;
+}
